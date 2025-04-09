12a0fd7118d74d0ce5a5d5b3b73f331401a5bc62
KikiopeShodipe <gilliansifuma@gmail.com>
2025-04-09 17:47:57 +0000

    Patterns

---
+++ main.py
@@ -0,0 +1,10 @@
+# Take input
+n = int(input("Enter the number of rows:"))
+# Outer loop to handle number of rows
+for i in range(n):
+    #Inner loop to handle number of columns
+    for j in range(i + 1):
+        #Display result
+        print("* ", end="")
+    # Newline after each row
+    print()
---
+++ main3.py
@@ -0,0 +1,17 @@
+rows = int(input("Enter the number of rows: "))
+# Upper half of the diamond
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
+# Lower half of the diamond
+for i in range(rows - 1, 0, -1):
+    print(" " * (rows - i), end="")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
